083cd99fde6799f457ba8753d0b60b5f25481df5
kewen <kewen@komoxo.com>
2020-08-31 16:28:12 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+#include <string>
+#include <vector>
+
+
+#include "fmt/core.h"
+#include "fmt/chrono.h"
+
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
 
@@ -12,6 +19,16 @@ int main() {
   filelog.get()->info("Sample Info output.");
   filelog.get()->warn("Sample Warn output.");
   filelog.get()->error("Sample Error output.");
+
+  // std::string s = fmt::format("I'd rather be {1} than {0}.", "right", "happy");
+
+  fmt::print("Hello world\n");
+
+  // using namespace std::literals::chrono_literals;
+  // fmt::print("Default format: {} {}\n", 42s, 100ms);
+  // fmt::print("strftime-like format: {:%H:%M:%S}\n", 3h + 15min + 30s);
+  std::vector<int> v = {1, 2, 3};
+  // fmt::print("{}\n", v);
 
   return 0;
 }
